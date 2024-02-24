986b3c9a2dd88ac556c33cf11ae02fe814288b04
Prakhar Sharma <prakharsharma2002s@gmail.com>
2024-02-24 14:26:51 +0530

    commit changes

---
+++ script.js
@@ -0,0 +1,10 @@
+const myDiv = document.getElementById("myDiv");
+
+myDiv.style.fontSize = "18px";
+myDiv.style.fontWeight = "bold";
+myDiv.style.color = "blue";
+myDiv.style.padding = "10px";
+myDiv.style.margin = "20px";
+myDiv.style.width = "300px";
+myDiv.style.height = "200px";
+myDiv.style.backgroundColor = "yellow";
